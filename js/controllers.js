d318af8f9f892c81a3c1d6937bd15351fadcb636
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2015-07-21 00:12:24 -0300

    starter

---
+++ js/controllers.js
@@ -0,0 +1,103 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+
+.controller('CardsCtrl', function ($scope, $http, $ionicLoading, $ionicSideMenuDelegate, TDCardDelegate) {
+  console.log('CARDS CTRL');
+  $ionicSideMenuDelegate.canDragContent(false);
+  var cardTypes = [];
+  $ionicLoading.show();
+  $http.get('https://randomuser.me/api/?results=5').success(function (response) {
+      angular.forEach(response.results, function (famous) {
+        cardTypes.push(famous);
+        //console.log(JSON.stringify(famous));
+      });
+      $ionicLoading.hide();
+    }).error(function (err) {
+      console.log(err);
+    });
+
+  //$scope.cards = Array.prototype.slice.call(cardTypes, 0);
+  $scope.cards = cardTypes;
+  $scope.cardDestroyed = function(index) {
+    $scope.cards.splice(index, 1);
+  };
+
+  $scope.addCard = function() {
+    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    newCard.id = Math.random();
+    $scope.cards.push(angular.extend({}, newCard));
+  }
+
+  $scope.yesCard = function() {
+    console.log('YES');
+    $scope.addCard();
+  };
+
+  $scope.noCard = function() {
+    console.log('NO');
+    $scope.addCard();
+  };
+  $scope.toggleLeft = function() {
+  $ionicSideMenuDelegate.toggleLeft();
+  };
+})
+.controller('CardCtrl', function($scope, TDCardDelegate) {
+  $scope.cardSwipedLeft = function(index) {
+    console.log('LEFT SWIPE');
+    $scope.addCard();
+  };
+  $scope.cardSwipedRight = function(index) {
+    console.log('RIGHT SWIPE');
+    $scope.addCard();
+  };
+})
+
+;
